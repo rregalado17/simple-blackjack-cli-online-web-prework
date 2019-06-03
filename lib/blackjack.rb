1729f0f40a3a599f3f51101cce082f5e30dc821b
magical-flow-3186 <rregalado17@gmail.com>
2019-06-03 04:37:29 +0000

    Done.

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,37 +1,56 @@
 def welcome
-  # code #welcome here
+  puts "Welcome to the Blackjack Table"
 end
 
-def deal_card
+def deal_card()
+  card = rand(1...11)
   # code #deal_card here
 end
 
-def display_card_total
+def display_card_total(card_total)
+ puts "Your cards add up to #{card_total}"
+ 
   # code #display_card_total here
 end
 
 def prompt_user
+puts "Type 'h' to hit or 's' to stay"
   # code #prompt_user here
 end
 
 def get_user_input
-  # code #get_user_input here
+user_input = gets.chomp
+
 end
 
-def end_game
-  # code #end_game here
+def end_game(card_total)
+puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
 
 def initial_round
-  # code #initial_round here
+card1 = deal_card
+card2 = deal_card
+card_total = (card1 + card2)
+display_card_total(card_total)
+return card_total
 end
 
-def hit?
-  # code hit? here
+def hit?(card_total)
+prompt_user
+user_input = get_user_input
+if user_input == "h"
+  card_total += deal_card
+elsif user_input == "s"
+  card_total
+else 
+  invalid_command
+end
 end
 
 def invalid_command
-  # code invalid_command here
+puts "Invald Command!"
+prompt_user
+get_user_input
 end
 
 #####################################################
@@ -39,6 +58,12 @@ end
 #####################################################
 
 def runner
-  # code runner here
+welcome
+card_total = initial_round
+until card_total > 21 
+  card_total = hit?(card_total)
+    display_card_total(card_total)
+end
+end_game(card_total)
 end
     
